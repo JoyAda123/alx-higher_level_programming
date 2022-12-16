545e51539c2bb39a00dbbe5838df7120acdc569e
Joy Ada Obekpa <joyobekpa29@gmail.com>
2022-12-16 09:03:18 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that sends a search request to the
-Star Wars API, searching people
-"""
-import requests
-import sys
+""" Github authentication script """
+from sys import argv
+from requests import get
+from requests.auth import HTTPBasicAuth
 
-
-if __name__ == "__main__":
-    search = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
-    r = requests.get(search)
-    json_o = r.json()
-    print("Number of results: {}".format(json_o.get('count')))
-    for character in json_o.get('results'):
-        print(character.get('name'))
+if __name__ == '__main__':
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    res = get("https://api.github.com/user", auth=auth)
+    print(res.json().get('id'))
